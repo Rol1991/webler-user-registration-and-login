2f0bb57ba455c128e610dfcc8a4f553e1a9d0d4f
Szőke Roland <szokeroland0@gmail.com>
2024-02-17 09:52:04 +0100

    Initial commit

---
+++ src/main/java/hu/webler/webleruserregistrationandlogin/WeblerUserRegistrationAndLoginApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.webleruserregistrationandlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerUserRegistrationAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerUserRegistrationAndLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webler/webleruserregistrationandlogin/WeblerUserRegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.webleruserregistrationandlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerUserRegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
